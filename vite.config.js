46463675766373f5ff28869c0b3c5388944afeb6
Sudip Kandel <sudipkandel27@gamil.com>
2025-06-26 08:15:46 +0545

    search

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,17 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import Sitemap from "vite-plugin-sitemap";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    Sitemap({
+      hostname: "https://sudip-kandel.com.np",
+    }),
+  ],
   server: {
     open: true,
     port: 3000,
