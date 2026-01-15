9d9d2748db7f92ad54c4d834819e84257f0f5b0b
sudip-kandel7 <sudipkandel27@gmail.com>
2026-01-15 10:51:28 +0545

    project data only

---
+++ src/data/projectData.js
@@ -0,0 +1,86 @@
+import blogger from "../assets/img-projects/blogger.png";
+import gym from "../assets/img-projects/gym.png";
+import weather from "../assets/img-projects/weather.png";
+import ttt from "../assets/img-projects/tic-tac-toe.png";
+import weather from "../assets/img-projects/weather.png";
+import simon from "../assets/img-projects/simon.png";
+import calcy from "../assets/img-projects/calcy.png";
+import todo from "../assets/img-projects/todo.png";
+import tenzies from "../assets/img-projects/tenzies.png";
+
+export const projects = [
+  {
+    title: "Bloggers - Js",
+    description:
+      "Made while learning Js and ejs. You can create posts.You can also update and delete.",
+    image: blogger.src,
+    tecnologies: ["HTML", "CSS", "Node.js", "Ejs"],
+    demo: "https://blog-project-7o5p.onrender.com",
+    code: "https://github.com/sudip-kandel7/blog-project.git",
+  },
+  {
+    title: "Gym - HTML CSS",
+    description: "Made while learning HTML and CSS.",
+    image: gym.src,
+    tecnologies: ["HTML", "CSS"],
+    demo: "https://sudip-kandel7.github.io/gym-website/",
+    code: "https://github.com/sudip-kandel7/gym-website.git",
+  },
+  {
+    title: "Weather Website",
+    description:
+      "A simple weather website that shows the weather of any city using open weather API. In default shows the weather of Pokhara.",
+    image: weather.src,
+    tecnologies: ["React", "CSS", "API"],
+    demo: "http://sudip-kandel.com.np/weather/",
+    code: "https://github.com/sudip-kandel7/weather.git",
+  },
+  {
+    title: "Tic-Tac-Toe Game",
+    description: "A simple Tic-Tac-Toe game made using JAVA.",
+    image: ttt.src,
+    tecnologies: ["JAVA", "XML"],
+    code: "https://github.com/sudip-kandel7/Tic-Tac-Toe.git",
+  },
+  {
+    title: "Book Review website",
+    descripcion: "Can search books, read reviews and add your own reviews.",
+    tecnologies: ["HTML", "Ejs", "Postgres"],
+    code: "https://github.com/sudip-kandel7/book-review.git",
+  },
+  {
+    title: "Simon Game",
+    descripcion:
+      "Web site for playing the classic Simon game. Made while learning JavaScript.",
+    image: simon.src,
+    tecnologies: ["HTML", "CSS", "Js"],
+    demo: "https://sudip-kandel7.github.io/Simon-Game/",
+    code: "https://github.com/sudip-kandel7/Simon-Game",
+  },
+  {
+    title: "Cacly",
+    descripcion:
+      "Calcy is a simple calculator app that performs basic arithmetic operations like addition, subtraction, multiplication, and division.Made using React.",
+    image: calcy.src,
+    tecnologies: ["React"],
+    demo: " https://sudip-kandel7.github.io/calcy/",
+    code: "https://github.com/sudip-kandel7/calcy.git",
+  },
+  {
+    title: "To-do List",
+    descripcion: "A simple to-do list website. Made using React.",
+    image: todo.src,
+    tecnologies: ["React", "CSS"],
+    demo: "https://sudip-kandel7.github.io/project-one/",
+    code: "https://github.com/sudip-kandel7/project-one.git",
+  },
+  {
+    title: "Tenzies Game",
+    descripcion:
+      "A simple Tenzies game made using React. Roll until all dice are the same. Click each die to freeze it at its current value between rolls.",
+    image: tenzies.src,
+    tecnologies: ["React", "CSS"],
+    demo: "https://sudip-kandel.com.np/Tenzies-game/",
+    code: "https://github.com/sudip-kandel7/Tenzies-game.git",
+  },
+];
---
+++ src/data/projectsAboutData.js
@@ -0,0 +1,41 @@
+import blogger from "../assets/img-projects/blogger.png";
+import gym from "../assets/img-projects/gym.png";
+import weather from "../assets/img-projects/weather.png";
+import ttt from "../assets/img-projects/tic-tac-toe.png";
+import weather from "../assets/img-projects/weather.png";
+
+export const projects = [
+  {
+    title: "Bloggers - Js",
+    description:
+      "Made while learning Js and ejs. You can create posts.You can also update and delete.",
+    image: blogger.src,
+    tecnologies: ["HTML", "CSS", "Node.js", "Ejs"],
+    demo: "https://blog-project-7o5p.onrender.com",
+    code: "https://github.com/sudip-kandel7/blog-project.git",
+  },
+  {
+    title: "Gym - HTML CSS",
+    description: "Made while learning HTML and CSS.",
+    image: gym.src,
+    tecnologies: ["HTML", "CSS"],
+    demo: "https://sudip-kandel7.github.io/gym-website/",
+    code: "https://github.com/sudip-kandel7/gym-website.git",
+  },
+  {
+    title: "Weather Website",
+    description:
+      "A simple weather website that shows the weather of any city using open weather API. In default shows the weather of Pokhara.",
+    image: weather.src,
+    tecnologies: ["React", "CSS", "API"],
+    demo: "http://sudip-kandel.com.np/weather/",
+    code: "https://github.com/sudip-kandel7/weather.git",
+  },
+  {
+    title: "Tic-Tac-Toe Game",
+    description: "A simple Tic-Tac-Toe game made using JAVA.",
+    image: ttt.src,
+    tecnologies: ["JAVA", "XML"],
+    code: "https://github.com/sudip-kandel7/Tic-Tac-Toe.git",
+  },
+];
